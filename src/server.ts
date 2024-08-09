ec69015917a9831830a51080bf6701487f7335c9
emanuele-rosa <emanuele.rosa@startse.com>
2024-08-09 17:11:53 -0300

    feat: create transaction

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions").select("*");
+app.register(transactionRoutes);
 
-  return transaction;
-});
 app
   .listen({
     port: env.PORT,
