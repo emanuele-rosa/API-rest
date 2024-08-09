73a93177d38a1c6d81c27894cb834b4a4de49f55
emanuele-rosa <emanuele.rosa@startse.com>
2024-08-09 17:19:26 -0300

    feat: create typing on knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+import { Knex } from "knex";
+
+
+declare module "knex/types/tables"{
+    export interface Tables {
+        transactions: {
+            id: string;
+            title: string;
+            amount: number;
+            createdAt: string;
+            session_id?: string;
+        }
+    }
+}
